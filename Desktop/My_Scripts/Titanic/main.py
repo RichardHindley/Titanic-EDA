11aa5debf8acdfaeb968ddae89f3b8d6ab40c782
RichardHindley <richardhindley@gmail.com>
2024-05-23 12:05:15 +0100

    Initial commit with project setup and basic EDA code

---
+++ Desktop/My_Scripts/Titanic/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Load data
+df = pd.read_csv ('/Users/richardhindley/Desktop/My_Scripts/Titanic/titanic_data/train.csv')
+
+#Display head
+print(df.head())
+
+#Data Cleaning and Analysis
+
+#Visualisations
+
+plt.show()
